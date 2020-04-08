50ae862d7badf9e4289e594e634253a1f4483e67
Victor Zubrinkin <zubr@rambler.ru>
2020-04-08 20:52:09 +0300

    refactor

--- src/03-sum-array/sum-array.ts
+++ src/03-sum-array/sum-array.ts
@@ -7,12 +7,8 @@ export const sumArray = (arr: number[]): number => {
   return sum;
 };
 
-export const recursiveSumArray = (arr: number[]): number => {
-  if (arr.length === 0) {
-    return 0;
-  }
-
-  return arr[0] + recursiveSumArray(arr.slice(1));
-};
+export const recursiveSumArray = (arr: number[]): number => (
+  arr.length ? arr[0] + recursiveSumArray(arr.slice(1)) : 0
+);
 
 export const reducedSumArray = (arr: number[]): number => arr.reduce((sum, item) => sum + item);
