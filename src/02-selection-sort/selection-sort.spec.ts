69555aa568cd72eb79f1e6ab8b8d1707bef19cf8
Victor Zubrinkin <zubr@rambler.ru>
2020-04-08 20:10:28 +0300

    refactoring, add test

--- src/02-selection-sort/selection-sort.spec.ts
+++ src/02-selection-sort/selection-sort.spec.ts
@@ -1,13 +1,13 @@
 import selectionSort from './selection-sort';
 
-describe('Selection sor testing', () => {
+describe('Selection sort testing', () => {
   test('[5, 7, 9, 2] should be sorted to [2, 5, 7, 9]', () => {
-    expect(selectionSort([5, 7, 9, 2])).toStrictEqual([2, 5, 7, 9]);
+    expect(selectionSort([5, 7, 9, 2])).toEqual([2, 5, 7, 9]);
   });
   test('[1, 2, 3, 4] should be same', () => {
-    expect(selectionSort([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4]);
+    expect(selectionSort([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
   });
-  test('[1, 1, 1, 1] should be same', () => {
-    expect(selectionSort([1, 1, 1, 1])).toStrictEqual([1, 1, 1, 1]);
+  test('[-1, 120, 15, -7] should be [-7, -1, 15, 120]', () => {
+    expect(selectionSort([-1, 120, 15, -7])).toEqual([-7, -1, 15, 120]);
   });
 });
