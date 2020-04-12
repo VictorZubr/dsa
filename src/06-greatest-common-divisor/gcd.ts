c18696b327da04b60263460cf8fc29040292243d
Victor Zubrinkin <zubr@rambler.ru>
2020-04-13 00:20:18 +0300

    refactor

--- src/06-greatest-common-divisor/gcd.ts
+++ src/06-greatest-common-divisor/gcd.ts
@@ -2,6 +2,5 @@ const gcd = (first: number, second: number): number => (
   second ? gcd(second, first % second) : first
 );
 
-export default (arr: number[]): number => arr
-  .slice(1)
-  .reduce((result, item) => gcd(result, item), arr[0]);
+export default ([first, ...otherItems]: number[]): number => otherItems
+  .reduce((result, item) => gcd(result, item), first);
