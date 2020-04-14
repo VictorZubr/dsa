bf5af53addd4a4a5b7f98dd61f784feea79a77cb
Victor Zubrinkin <zubr@rambler.ru>
2020-04-14 20:01:13 +0300

    refactor

--- src/07-breadth-first-search/breadth-first-search.ts
+++ src/07-breadth-first-search/breadth-first-search.ts
@@ -2,7 +2,11 @@ export interface Graph {
   [key:string]: string[];
 }
 
-export default (name: string, graph: Graph, targetFunction: Function): string | boolean => {
+export default (
+  name: string,
+  graph: Graph,
+  targetFunction: (name: string) => boolean,
+): string | boolean => {
   const queue: string[] = (graph[name]).slice();
   const visited: string[] = [];
   while (queue.length) {
