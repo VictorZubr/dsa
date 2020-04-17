2ed86707ff7a2792fe05497c7f1cedba8709b010
Victor Zubrinkin <zubr@rambler.ru>
2020-04-17 18:45:57 +0300

    refactor quick sort

--- src/05-quick-sort/quick-sort.ts
+++ src/05-quick-sort/quick-sort.ts
@@ -1,12 +1,21 @@
+const splitByPivot = (arr: number[], pivot: number): [number[], number[]] => arr
+  .reduce((acc: [number[], number[]], item) => {
+    if (item > pivot) {
+      acc[1].push(item);
+    } else {
+      acc[0].push(item);
+    }
+
+    return acc;
+  }, [[], []]);
+
 // eslint-disable-next-line import/prefer-default-export
 export const quickSort = (arr: number[]): number[] => {
   if (arr.length < 2) {
     return arr;
   }
-  const pivot = arr[0];
-  const restItems = arr.slice(1);
-  const smallerItems = restItems.filter((item) => item <= pivot);
-  const greaterItems = restItems.filter((item) => item > pivot);
+  const [pivot, ...restItems] = arr;
+  const [smallerItems, greaterItems] = splitByPivot(restItems, pivot);
 
   return [...quickSort(smallerItems), pivot, ...quickSort(greaterItems)];
 };
