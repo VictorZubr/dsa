0ba25589318de04a4523ea4dbe821fa5442e9b6a
Victor Zubrinkin <zubr@rambler.ru>
2020-04-22 20:54:16 +0300

    refactor of dijkstra algorithm to resolve ESLint problem

--- src/08-dijkstra-algorithm/dijkstra-algorithm.ts
+++ src/08-dijkstra-algorithm/dijkstra-algorithm.ts
@@ -15,29 +15,37 @@ export interface NodeCost {
   cost: number
 }
 
+const getLowestCostNode = (ribsCosts: Ribs, processed: string[]): NodeCost => Object
+  .entries(ribsCosts).reduce((acc, [key, rate]) => (rate < acc.cost && !processed.includes(key)
+    ? { node: key, cost: rate }
+    : acc), { node: null, cost: Infinity } as NodeCost);
+
+const getNewCostsAndParents = (
+  node: string,
+  graph: Graph,
+  costs: Ribs,
+  cost: number,
+): {newCosts: Ribs, newParents: Parents } => {
+  const neighbors = graph[node];
+  return Object.keys(neighbors).reduce(({ newCosts, newParents }, key) => {
+    const newCost = cost + neighbors[key];
+
+    return (newCost < costs[key])
+      ? { newCosts: { ...newCosts, [key]: newCost }, newParents: { ...newParents, [key]: node } }
+      : { newCosts, newParents };
+  }, { newCosts: {}, newParents: {} });
+};
+
 export default (graph: Graph, costs: Ribs, parents: Parents): Ribs => {
   const processed: string[] = [];
 
-  const getLowestCostNode = (ribsCosts: Ribs): NodeCost => Object
-    .entries(ribsCosts).reduce((acc, [key, rate]) => (rate < acc.cost && !processed.includes(key)
-      ? { node: key, cost: rate }
-      : acc), { node: null, cost: Infinity } as NodeCost);
-
-  let { node, cost } = getLowestCostNode(costs);
+  let { node, cost } = getLowestCostNode(costs, processed);
   while (node) {
-    const neighbors = graph[node];
-    // TODO refactor of function to resolve ESLint problem
-    // eslint-disable-next-line no-loop-func
-    Object.keys(neighbors).forEach((key) => {
-      const newCost = cost + neighbors[key];
-      if (newCost < costs[key]) {
-        Object.assign(costs, { [key]: newCost });
-        Object.assign(parents, { [key]: node });
-      }
-    });
-
+    const { newCosts, newParents } = getNewCostsAndParents(node, graph, costs, cost);
+    Object.assign(costs, newCosts);
+    Object.assign(parents, newParents);
     processed.push(node);
-    ({ node, cost } = getLowestCostNode(costs));
+    ({ node, cost } = getLowestCostNode(costs, processed));
   }
 
   return costs;
