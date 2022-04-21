629e882296fe2ee677fce685670d4e01429fd158
Victor Zubrinkin <zubr@rambler.ru>
2022-04-21 14:23:52 +0300

    refactor

--- src/roughgarden/basics/01-mult/mult-by-column.ts
+++ src/roughgarden/basics/01-mult/mult-by-column.ts
@@ -2,11 +2,11 @@ const DECIMAL = 10;
 
 const rowReducer = (a: string) => (
   rowResult: number, b: string, positionB: number,
-): number => rowResult + Number(b) * Number(a) * 10 ** positionB;
+): number => rowResult + Number(b) * Number(a) * DECIMAL ** positionB;
 
 const multiplication = (arr: Array<string>) => (
   multResult: number, a: string, positionA: number,
-): number => multResult + arr.reduce(rowReducer(a), 0) * 10 ** positionA;
+): number => multResult + arr.reduce(rowReducer(a), 0) * DECIMAL ** positionA;
 
 
 // multiplication by column
